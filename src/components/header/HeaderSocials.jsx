9a0074f6daad03509f1e8b4d935bf33e48d21319
sursindmitry <dmitrijsursin@gmail.com>
2022-05-15 23:04:05 +0700

    Add Header

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+import { BsTwitter } from 'react-icons/bs'
+
+
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href='https://twitter.com' target="_blank"><BsTwitter/></a>
+        <a href='https://github.com' target="_blank"><FaGithub/></a>
+        <a href='https://www.linkedin.com/' target="_blank"><BsLinkedin/></a>
+
+    </div>
+  )
+}
+
+export default HeaderSocials
