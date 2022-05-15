ad1d4d248736b29132311131dbbc25bff66c03c8
sursindmitry <dmitrijsursin@gmail.com>
2022-05-15 19:58:37 +0700

    add components

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/sevices/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contacts from './components/contact/Contacts'
+import Footer from './components/footer/Footer'
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contacts />
+      <Footer />
+    </>
+  )
+}
+
+export default App
